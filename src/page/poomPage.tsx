111cf31be911feb8e3ffcfa99fdeb497a53c4dcf
Meawjai <wananuthida.46@gmail.com>
2025-10-01 17:38:02 +0700

    mmmm

--- src/page/poomPage.tsx
+++ src/page/poomPage.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 function poomPage() {
   return (
-<<<<<<< HEAD
-    <div>poomPage</div>
-=======
     <div>poomPage page2 023</div>
->>>>>>> 06050d5442affa1d15f364d7bd7a015bcc81ee69
   )
 }
 
